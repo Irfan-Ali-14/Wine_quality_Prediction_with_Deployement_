ba367071bd8230f91e4404c7cfeb59501d9fdb61
IrfanAli <Irfanbalti14@gmail.com>
2022-06-08 14:51:04 +0500

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", 
+    version= "0.0.1",
+    description="it is a wine q pakaeges",
+    author= "Irfan-Ali-14",
+    packages=find_packages(),
+    license= "MIT"
+
+)
+
